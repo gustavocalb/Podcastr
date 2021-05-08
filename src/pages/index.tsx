55111a10de9f8b2dff7d8477b6f304840247c1c6
FireShark688 <gugacalb@gmail.com>
2021-05-07 21:14:44 -0300

    .

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+export default function Home() {
+  return (
+   <>
+    <h1>Index</h1>
+   </>
+  )
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+  
+  return { 
+    props: {
+      episodes: data,
+    },
+
+    revalidate: 60 * 60 * 8,
+  }
+}
